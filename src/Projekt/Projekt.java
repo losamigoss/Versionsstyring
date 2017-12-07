f5ff6f6a91dd323618bb4c3031722bb43bc7b104
Andreas Peter Jørgensen <andreasjsen@live.dk>
2017-12-07 16:45:59 +0100

    Tjek om myndighed

--- src/Projekt/Projekt.java
+++ src/Projekt/Projekt.java
@@ -1,5 +1,21 @@
 package Projekt;
 
-public class Projekt {
+import java.util.Scanner;
 
+public class Projekt {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		System.out.println("Indtast venligst din alder - så svarer jeg om du er myndig endnu");
+		
+		int alder = scan.nextInt();
+			
+		if (alder<18) {
+			System.out.println("Du er ikke myndig");
+		}
+		if (alder>=18) {
+			System.out.println("Du er myndig");
+		}
+
+		
+}
 }
